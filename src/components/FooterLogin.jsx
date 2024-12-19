9210f606a0b5c87a7f3f501d2403eb8a3c9b17e4
Moch Achmar <iyekodong86@gmail.com>
2024-12-19 20:56:04 +0800

    memperbarui file

--- src/components/FooterLogin.jsx
+++ src/components/FooterLogin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/Logo.png'; // Make sure this path is correct
+import logo from '../assets/logo.png'; // Make sure this path is correct
 
 const Footer = () => {
   return (
